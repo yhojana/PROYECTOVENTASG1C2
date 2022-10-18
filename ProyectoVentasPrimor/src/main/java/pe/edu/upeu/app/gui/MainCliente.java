152e31fef6fee21078272b830a450e1ba604bdb7
Usuario <Usuario@DESKTOP-L8IGU86>
2022-10-17 20:57:00 -0500

    CRUD completado

--- ProyectoVentasPrimor/src/main/java/pe/edu/upeu/app/gui/MainCliente.java
+++ ProyectoVentasPrimor/src/main/java/pe/edu/upeu/app/gui/MainCliente.java
@@ -65,7 +65,7 @@ public class MainCliente extends javax.swing.JPanel {
             txtNombre.setText(d.getNombrers());
             cbxTipo.setSelectedItem(d.getTipo());
             txtDni.setEditable(false);
-            btnRegistrar.setText("MODIFICAR");
+            btnRegistrar.setText("CAMBIAR");
             //guardarButton.setToolTipText("MODIFICAR");
         } else {
             txtDni.setEditable(true);
